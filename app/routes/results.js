446d79e463650616398fc4eecc7af5cb6adbd18c
Neil Larion <neil.larion@gmail.com>
2016-04-05 11:08:43 -0700

    Push responce from youtube to array for each congress person and wait to finish before returning all data

--- app/routes/results.js
+++ app/routes/results.js
@@ -4,16 +4,23 @@ export default Ember.Route.extend({
   model: function(params) {
     var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=ead3432ff97d4c36b01cc6ce3c787482&zip=' + params.zip;
     return Ember.$.getJSON(url).then(function(responseJSON) {
+      var retArr = [];
       for (var i = 0; i < responseJSON.results.length; i++) {
         var youtubeUrl = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername="+responseJSON.results[i].youtube_id+"&key=AIzaSyBfS3lQCRNCju3YjNtXyDvsz3E2l3eMYxA";
         Ember.$.getJSON(youtubeUrl).then(function(responceYoutube){
-          console.log(responceYoutube.items[0].contentDetails.relatedPlaylists.uploads);
+          //console.log(responceYoutube.items[0].contentDetails.relatedPlaylists.uploads);
           var playlistUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId="+responceYoutube.items[0].contentDetails.relatedPlaylists.uploads+"&key=AIzaSyBfS3lQCRNCju3YjNtXyDvsz3E2l3eMYxA";
           Ember.$.getJSON(playlistUrl).then(function(responsePlaylist){
-            console.log(responsePlaylist);
+            //console.log(responsePlaylist);
+            retArr.push(responsePlaylist);
+            if(retArr.length === responseJSON.results.length){
+              console.log("i: "+i+" responseJSON.results.length: "+ responseJSON.results.length);
+              console.log(retArr);
+              return retArr;
+            }
           });
         });
-      }
+      }//end for loop
     });
   }
 });
