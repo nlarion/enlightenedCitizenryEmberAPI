e1b32ab3180c525a0ef1e9b37586fbd3a14182e9
Neil Larion <neil.larion@gmail.com>
2016-04-05 17:01:53 -0700

    Get the videos to load when a user clicks a congress persons name

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,21 +2,10 @@ import Ember from 'ember';
 
 
 export default Ember.Route.extend({
-  model: function(params) {
-    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=ead3432ff97d4c36b01cc6ce3c787482&zip=' + params.zip;
-    var cast = Ember.RSVP.Promise.cast.bind(Ember.RSVP.Promise);
-    return  cast(Ember.$.getJSON(url))
-    .then(function(response) {
-        console.log(response);
-        var youtubeUrl = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername="+response.results[0].youtube_id+"&key=AIzaSyBfS3lQCRNCju3YjNtXyDvsz3E2l3eMYxA";
-        var cast = Ember.RSVP.Promise.cast.bind(Ember.RSVP.Promise);
-        return  cast(Ember.$.getJSON(youtubeUrl))
-    })
-    .then(function(response) {
-        console.log(response);
-        var playlistUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId="+response.items[0].contentDetails.relatedPlaylists.uploads+"&key=AIzaSyBfS3lQCRNCju3YjNtXyDvsz3E2l3eMYxA";
-        var cast = Ember.RSVP.Promise.cast.bind(Ember.RSVP.Promise);
-        return  cast(Ember.$.getJSON(playlistUrl))
-    });
+ model: function(params) {
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=ead3432ff97d4c36b01cc6ce3c787482&zip=' + params.zip;
+   return Ember.$.getJSON(url).then(function(responseJSON) {
+     return responseJSON.results;
+   });
   }
 });
